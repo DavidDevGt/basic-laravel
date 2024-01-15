4e77d42b84ad21862b8f9a485dfe697ea1d53273
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-15 01:30:38 -0600

    Add view rendering to UserController index method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
     //
-    public function index(int $user_id): string {
+    public function index(int $user_id): view {
         // Aqui le metemos la logica de la aplicacion
         // usa las declaraciones de tipo
         
         // Para hacer un var_dump en laravel
         // dd($user_id);
 
-        return '<h1>Desde el controller users</h1>';
+        return view('welcome');
     }
 }
