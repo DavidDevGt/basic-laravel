72b08d82c09b1d45eb93fa1cff6ed502a8c64971
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-15 01:09:37 -0600

    Add UserController route for user details

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index(int $user_id) {
+        // Aqui le metemos la logica de la aplicacion
+        // usa las declaraciones de tipo
+        
+        // Para hacer un var_dump en laravel
+        // dd($user_id);
+
+        return '<h1>Desde el controller users</h1>';
+    }
+}
