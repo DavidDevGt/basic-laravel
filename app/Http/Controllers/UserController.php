754a53876760204b0a912b12da3815be44ed4df3
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-15 01:10:19 -0600

    Update UserController index method return type

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    public function index(int $user_id) {
+    public function index(int $user_id): string {
         // Aqui le metemos la logica de la aplicacion
         // usa las declaraciones de tipo
         
